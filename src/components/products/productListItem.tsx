0b8a5984a82607ab183a2f4079c562168a6f88e2
Tony.Wu <uq44961879@gmail.com>
2023-07-30 17:45:35 +1000

    <feat>: add sale tag with fixed position to the product list image

--- src/components/products/productListItem.tsx
+++ src/components/products/productListItem.tsx
@@ -24,6 +24,21 @@ class ProductListItem extends React.Component<Props, States> {
                     </Link>
                 </Container>
                 <Container style={{color:"#333", letterSpacing: "normal"}}> $ {this.props.info.originPrice} </Container>
+                {
+                    Math.floor(Math.random() * 2) === 1? 
+                    (<span style={{
+                        position: 'absolute',
+                        top: "0px",
+                        left: '0px',
+                        color:"white", 
+                        margin: "0px 0 0 24px",
+                        padding: "0 5px",
+                        borderRadius: "25px",
+                        backgroundColor: 'red',
+                        border: "1px solid red"}}> Sale  </span>):<span></span>
+                }
+                
+                
             </div>
         )
     }
