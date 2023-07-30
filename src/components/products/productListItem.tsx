8b773f24319509a0eeb810e7ce03e340aa5c0e8d
Tony.Wu <uq44961879@gmail.com>
2023-07-30 21:09:25 +1000

    <feat>: add palceholder for sale tag feature and its style.

--- src/components/products/productListItem.tsx
+++ src/components/products/productListItem.tsx
@@ -17,28 +17,24 @@ class ProductListItem extends React.Component<Props, States> {
     render() {
         return (
             <div className='product-list-item'>
-                <img src={this.props.info?this.props.info.src:placeholderImage} width="100%" height="80%"/>
-                <Container>
                     <Link style={{textDecoration: 'none'}} to={this.props.info.productName.toString()}> 
-                        {this.props.info.productName} 
+                        {
+                            Math.floor(Math.random() * 3) === 1? 
+                            (<span style={{
+                                position: 'absolute',
+                                top: "0px",
+                                left: '0px',
+                                color:"white", 
+                                margin: "0px 0 0 24px",
+                                padding: "0 5px",
+                                borderRadius: "25px",
+                                backgroundColor: 'red',
+                                border: "1px solid red"}}> Sale  </span>):<span></span>
+                        }
+                        <img src={this.props.info?this.props.info.src:placeholderImage} width="100%" height="80%"/>
+                        <Container> {this.props.info.productName}  </Container>
+                        <Container style={{color:"#333", letterSpacing: "normal"}}> $ {this.props.info.originPrice} </Container>
                     </Link>
-                </Container>
-                <Container style={{color:"#333", letterSpacing: "normal"}}> $ {this.props.info.originPrice} </Container>
-                {
-                    Math.floor(Math.random() * 3) === 1? 
-                    (<span style={{
-                        position: 'absolute',
-                        top: "0px",
-                        left: '0px',
-                        color:"white", 
-                        margin: "0px 0 0 24px",
-                        padding: "0 5px",
-                        borderRadius: "25px",
-                        backgroundColor: 'red',
-                        border: "1px solid red"}}> Sale  </span>):<span></span>
-                }
-                
-                
             </div>
         )
     }
