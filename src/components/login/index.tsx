f5b4e6144ca960c67c8ea2a6999156dc5a98307a
Tony.Wu <uq44961879@gmail.com>
2023-08-02 06:13:57 +1000

    <feat>: add navigate to / after login form submitted

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,36 +1,43 @@
 import React, { FormEvent } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 import { useDocumentTitle } from '../../utils'
 import { AuthForm, useAuth } from '../../context'
 import { handleUserResponse } from '../../auth-provider'
 import { response } from 'msw'
 import { http } from '../../utils/http'
+import { Container, Button, TextField } from '@mui/material'
 
 const apiUrl = process.env.REACT_APP_API_URL
 
 export const LoginView = () => {
+
+    const navigate = useNavigate()
     useDocumentTitle("Login", false)
+    
     const { login, user, register, logout } = useAuth()
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event?.preventDefault()
         const username = (event.currentTarget.elements[0] as HTMLInputElement).value
         const password = (event.currentTarget.elements[1] as HTMLInputElement).value
         login({username, password})
+        navigate('/')
     }
     return (
         <>
-            <div> Hello, {user?.name} </div>
+<Container style={{display: 'flex', justifyContent: 'center'}}>
             <form onSubmit={handleSubmit}>
                 <div>
-                    <label htmlFor="username"> Username </label>
-                    <input type="text" id={'username'}/>
+                    <TextField id="standard-basic" label="Username" variant="standard" />
                 </div>
                 <div>
-                    <label> Password </label>
-                    <input type="password" id={'passoword'} />
+                    <TextField id="standard-basic" label="Password" variant="standard" type="password" />
                 </div>
-                <button type={"submit"}> Sign in </button>
+                <Button style={{margin: '15px 0', width: '100%'}} type={"submit"} variant="text"> Sign in </Button>
+                    
             </form>
-            <button onClick={() => {
+            
+        </Container>
+        <button onClick={() => {
                 fetch(`https://backend202307112242.azurewebsites.net/webapp/page2`).then(async response => {
                     if(response.ok) {
                         console.log(await response.json())
@@ -53,5 +60,6 @@ export const LoginView = () => {
                 })
             }}> Test API </button>
         </>
+        
     )
 }
